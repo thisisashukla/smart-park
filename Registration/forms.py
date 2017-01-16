20bb84c3fd458736e3e24dd0e5b1220046b2e068
Ankur Shukla <work.ankurshukla@gmail.com>
2017-01-17 00:19:03 +0530

    Trivia

--- Registration/forms.py
+++ Registration/forms.py
@@ -12,10 +12,10 @@ from Registration.models import Parking
 class User_Form(ModelForm):
     class Meta:
         model = User
-        fields = ['fullName', 'email', 'password', 'city']
+        fields = ['full_name', 'email', 'password', 'city']
         
 class Parking_Form(ModelForm):
     class Meta:
         model = Parking
-        fields=['ownerName','parkingName','capacity','lat','long']
+        fields=['owner_name','parking_name','capacity','lat','long']
         widgets = {'lat': forms.HiddenInput(),'long':forms.HiddenInput()}        
