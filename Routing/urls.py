25130b2362d921b048e117ab0c42595b6fcbd6fb
Ankur Shukla <work.ankurshukla@gmail.com>
2017-01-18 02:48:48 +0530

    Trivial

--- Routing/urls.py
+++ Routing/urls.py
@@ -8,7 +8,7 @@ from . import views
 
 urlpatterns=[
     url(r'^',views.Routing_Main,name='Routing_loc'),
-    url(r'(?P<num>[0-9]+)/(?P<co>\[.*\])',views.snap_coordinate,name='Routing_route'),
+    url(r'(?P<num>[0-9]+)/(?P<co>\[.*\])',views.snap_coordinate,name='Routing_snap'),
     url(r'(?P<source>[0-9]+)/(?P<target>[0-9]+)',views.route,name='Routing_route')
 ]
 
